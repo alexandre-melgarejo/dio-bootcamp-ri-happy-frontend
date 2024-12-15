84f43aff782168fb5c14b5197d58ae3864efc730
alexandre-melgarejo <ar.melgarejo@gmail.com>
2024-12-15 00:43:05 -0400

    feat: listagem pokedex ok

---
+++ construindo-pokedex-js/assets/js/main.js
@@ -0,0 +1,56 @@
+
+const maxRecords = 11;
+const limit = 10;
+let offset = 0;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMore');
+
+function convertPokemonTypesToHtml(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToHtml(pokemon) {
+    console.log(pokemon.types);
+    // debugger
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}" 
+                    alt="${pokemon.name}">
+            </div>
+        </li>
+`
+}
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertPokemonToHtml).join('');
+        pokemonList.innerHTML += newHtml;
+    })
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit;
+
+    const qtdRecordNextPage = offset + limit;
+
+    if (qtdRecordNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset;
+        loadPokemonItens(offset, newLimit);
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton);
+    } else {
+        loadPokemonItens(offset, limit);
+    }
+})
